36c80033be5d8f684520feadd7863e09fb7ea7fd
Sebastian Stein <seb.kde@hpfsc.de>
2026-02-21 19:44:25 +0100

    initial import

---
+++ src/photoaident/__main__.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6 import QtCore, QtWidgets
+
+from photoaident.app import MyWidget
+
+
+def main():
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
